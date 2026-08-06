e20738b50c3ef39358a52e43a41757706e11407c
yahyapro20 <hosseiniyahya96@gmail.com>
2026-08-06 15:39:37 +0330

    Reorder logging statements in OnCreate method
    Moved logging of SMAPI Loader start and device info to the beginning of OnCreate method.

--- SMAPIGameLoader/Launcher/LauncherActivity.cs
+++ SMAPIGameLoader/Launcher/LauncherActivity.cs
@@ -30,6 +30,9 @@ public class LauncherActivity : AppCompatActivity
     {
         Instance = this;
         base.OnCreate(savedInstanceState);
+        
+        LogTool.Info("SMAPI Loader started");
+        LogTool.Info($"Device: {Build.Manufacturer} {Build.Model}, Android {Build.VERSION.Release}");
 
         SetContentView(ResourceConstant.Layout.LauncherLayout);
 
@@ -45,9 +48,6 @@ public class LauncherActivity : AppCompatActivity
 
         //run utils scripts
         ProcessAdbExtras();
-
-        LogTool.Info("SMAPI Loader started");
-        LogTool.Info($"Device: {Build.Manufacturer} {Build.Model}, Android {Build.VERSION.Release}");
     }
 
     private void SetDarkMode()
